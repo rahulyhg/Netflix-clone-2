69ea36e9a54a5f8f65f4f1f24b985d7a97e17a51
Gustavo Lopez <amilcar.lopez@hotmail.es>
2016-02-02 16:45:30 -0600

    Final

--- views/register.php
+++ views/register.php
@@ -9,6 +9,7 @@
 	<link href='http://fonts.googleapis.com/css?family=Gochi+Hand' rel='stylesheet' type='text/css'>
 </head>
 <body>
+	<!-- encabezado donde se encuentra el logo y el iniciar sesion y el modal del inicio-->
 	<header>
 		<div class="container">
 			<div class="row">
@@ -51,77 +52,77 @@
 	</div>
 	</header>
 	<section>
-	</div>
+	<!-- contenor de los planes y opciones a adquirir-->
 		<div class="container">
 			<h1 class="col-xs-12">Seleciona el plan ideal para ti</h1>
 			<h4 class="hidden-xs col-md-12">Cambia a un plan inferior o superior en cualquier momento</h4>
 			
 			<table class="table ">
-    <thead>
-        <tr>
-        	<th class="text-center"></th>
-            <th class="text-center">BASICO</th>
-            <th class="text-center">ESTANDAR</th>
-            <th class="text-center">PREMIUM</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-        	<td class="text-center">El precio después de un mes gratis.</td>
-            <td class="text-center">$7.99</td>
-            <td class="text-center">$9.99</td>
-            <td class="text-center">$11.99</td>
-        </tr>
-        <tr>
-            <td class="text-center" >HD disponible.</td>
-            <td class="text-center"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></td>
-            <td class="text-center"> <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-            <td class="text-center"> <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-        </tr>
-        <tr>
-            <td class="text-center">Ultra HD</td>
-            <td class="text-center"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></td>
-            <td class="text-center"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></td>
-            <td class="text-center"> <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-        </tr>
-         <tr>
-            <td class="text-center">Pantallas en las que puedes ver al mismo tiempo</td>
-            <td class="text-center">1</span></td>
-            <td class="text-center">2</span></td>
-            <td class="text-center">4</span></td>
-        </tr>
-         
-         <tr>
-            <td class="text-center">Ve en tu laptop, TV, teléfono y tableta</td>
-            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-        </tr>
-        <tr>
-            <td class="text-center">Películas y series ilimitadas</td>
-            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-        </tr>
-        <tr>
-            <td class="text-center">Cancela en cualquier momento.</td>
-            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-            <td class="text-center" ><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-        </tr>
-        <tr>
-            <td class="text-center">El primer mes es gratis</td>
-           <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
-        </tr>
-        <tr>
-        	<td class="text-center">Suscribete para empezar tu mes gratis</td>
-        	<td><button class="btn btn-danger center-block" data-toggle="modal" data-target="#register">Basico</button></td>
-        	<td><button class="btn btn-danger center-block" data-toggle="modal" data-target="#register">Estandar</button></td>
-        	<td><button class="btn btn-danger center-block" data-toggle="modal" data-target="#register">Premium</button></td>
-        </tr>
-    </tbody>
+			    <thead>
+			        <tr>
+			        	<th class="text-center"></th>
+			            <th class="text-center">BASICO</th>
+			            <th class="text-center">ESTANDAR</th>
+			            <th class="text-center">PREMIUM</th>
+			        </tr>
+			    </thead>
+	    <tbody>
+	        <tr>
+	        	<td class="text-center">El precio después de un mes gratis.</td>
+	            <td class="text-center">$7.99</td>
+	            <td class="text-center">$9.99</td>
+	            <td class="text-center">$11.99</td>
+	        </tr>
+	        <tr>
+	            <td class="text-center" >HD disponible.</td>
+	            <td class="text-center"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></td>
+	            <td class="text-center"> <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	            <td class="text-center"> <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	        </tr>
+	        <tr>
+	            <td class="text-center">Ultra HD</td>
+	            <td class="text-center"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></td>
+	            <td class="text-center"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></td>
+	            <td class="text-center"> <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	        </tr>
+	         <tr>
+	            <td class="text-center">Pantallas en las que puedes ver al mismo tiempo</td>
+	            <td class="text-center">1</span></td>
+	            <td class="text-center">2</span></td>
+	            <td class="text-center">4</span></td>
+	        </tr>
+	         
+	         <tr>
+	            <td class="text-center">Ve en tu laptop, TV, teléfono y tableta</td>
+	            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	        </tr>
+	        <tr>
+	            <td class="text-center">Películas y series ilimitadas</td>
+	            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	        </tr>
+	        <tr>
+	            <td class="text-center">Cancela en cualquier momento.</td>
+	            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	            <td class="text-center" ><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	        </tr>
+	        <tr>
+	            <td class="text-center">El primer mes es gratis</td>
+	           <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	            <td class="text-center"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+	        </tr>
+	        <tr>
+	        	<td class="text-center">Suscribete para empezar tu mes gratis</td>
+	        	<td><button class="btn btn-danger center-block" data-toggle="modal" data-target="#register">Basico</button></td>
+	        	<td><button class="btn btn-danger center-block" data-toggle="modal" data-target="#register">Estandar</button></td>
+	        	<td><button class="btn btn-danger center-block" data-toggle="modal" data-target="#register">Premium</button></td>
+	        </tr>
+	    </tbody>
 </table>
 		<br>
 		<small class="col-xs-12">*Ultra Hd solo esta disponible en plan de 4 pantallas +HD. La disponibilidad del contenido en alta deficinicion
